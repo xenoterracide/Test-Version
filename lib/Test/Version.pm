afee0801ef902fbac378fcb81e6bd2f37577796b
Caleb Cushing <xenoterracide@gmail.com>
2011-05-26 09:03:04 -0500

    dzil-ify Test::Version
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Test/Version.pm
+++ lib/Test/Version.pm
@@ -28,15 +28,15 @@ sub import
 
     $Test->exported_to( $caller );
 }
-    
+
 sub version_ok
 {
     my( $file, $expected, $name ) = @_;
     $expected ||= OK;
     $name ||= qq{VERSION test for $file};
-    
+
     my $status = _check_version( $file );
-            
+
     if( defined $expected and $expected eq $status )
     {
         $Test->ok( 1, $name )
@@ -65,15 +65,15 @@ sub version_ok
 sub _check_version
 {
     my $file = shift;
-    
+
     return NO_FILE unless -e $file
         and $file =~ m/(.+)\.pm\z/;
 
     require $file;
-    
+
     my $package = $1;
     $package =~ s/\//::/g;
-    
+
     no strict 'refs';
     my $output = ${ $package . '::VERSION' };
     use strict 'refs';
@@ -81,28 +81,21 @@ sub _check_version
     return NO_VERSION unless $output;
     return OK;
 }
-        
+
 
 $_ ^=~ { module => q{Test::Version}, author => q{particle} };
 
 __END__
 
-=head2 NAME
-
-Test::Version - check for VERSION information in modules
-
-=head2 VERSION
-
-This document describes version 0.02 of Test::Version,
-released 18 November 2002.
+# ABSTRACT: check for VERSION information in modules
 
 =head2 SYNOPSIS
 
-use Test::Version;
+	use Test::Version;
 
-plan tests => $num_tests;
+	plan tests => $num_tests;
 
-version_ok( $file );
+	version_ok( $file );
 
 =head2 DESCRIPTION
 
@@ -136,6 +129,8 @@ The optional third argument NAME is the name of the test
 which version_ok passes through to Test::Builder.  Otherwise,
 it choose a default test name "VERSION test for FILENAME".
 
+=back
+
 =head2 CAVEATS
 
 Currently only checks files ending in '.pm', and expects the package name to match the filename. I'm open to suggestions for more robust parsing.
@@ -147,24 +142,8 @@ When sending bug reports, please provide the version of Test::Version, the
 version of Perl, and the name and version of the operating system you are
 using.
 
-=head2 AUTHOR
-
-particle, E<lt>particle@cpan.orgE<gt>
-
-=head2 COPYRIGHT
-
-Copyright 2002, Ars ex Machina, Corp. All rights reserved.
-
-This package is free software and is provided "as is" without express or
-implied warranty. It may be used, redistributed and/or modified under the terms
-of the Perl Artistic License (see http://www.perl.com/perl/misc/Artistic.html)
-
 =head2 CREDITS
 
 Thanks to brian d foy for the inspiration -- his Test::Pod module (on which this code is based,) and his "Better Documentation Through Testing" article in The Perl Journal, Nov 2002 (see http://www.tpj.com/).
-
-=head2 SEE ALSO
-
-L<Test::Pod>
 
 =cut
